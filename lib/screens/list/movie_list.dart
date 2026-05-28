d6b63be2fa92951cbf6b20abc018da85ef79641f
Matt Solle <sollematthew@gmail.com>
2026-05-28 16:25:48 -0400

    feat: finish listview

--- lib/screens/list/movie_list.dart
+++ lib/screens/list/movie_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widget_previews.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:studioghibli/core/injectables.dart';
@@ -56,90 +55,79 @@ class ListScreenLoaded extends StatelessWidget {
 
   final MovieStateLoaded state;
 
-  /*
-  final String id;
-  final String title;
-  final String originalTitle;
-  final String originalTitleRomanised;
-  final String description;
-  final String director;
-  final String producer;
-  final String releaseDate;
-  // Running Time, in minutes
-  final String runningTime;
-  final String rtScore;
-  final List<String> people;
-  final List<String> species;
-  final List<String> locations;
-  final List<String> vehicles;
-  final String url;
-  */
-
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
         const SliverAppBar(
           title: Text('Studio Ghibli'),
+          pinned: true,
         ),
         SliverList(
           delegate: SliverChildBuilderDelegate((context, index) {
             final movie = state.movies[index];
 
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: .start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: .center,
-                      children: [
-                        Text(
-                          movie.title,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
+            return GestureDetector(
+              onTap: () {
+                print(movie.id);
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: .start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: .center,
+                        children: [
+                          Text(
+                            movie.title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: .center,
-                      children: [
-                        Expanded(
-                          child: Text(
-                            '${movie.originalTitle} (${movie.originalTitleRomanised})',
-                            maxLines: 2,
-                            softWrap: true,
-                            textAlign: .center,
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: .center,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              '${movie.originalTitle} (${movie.originalTitleRomanised})',
+                              maxLines: 2,
+                              softWrap: true,
+                              textAlign: .center,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
+                        ],
+                      ),
 
-                    const SizedBox(height: 16),
-                    Text(
-                      movie.description,
-                      maxLines: 8,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    const SizedBox(height: 16),
-                    Row(
-                      mainAxisAlignment: .center,
-                      children: [
-                        Expanded(
-                          child: Text(
-                            '${movie.releaseDate} | ${movie.director} | ${movie.producer}',
-                            maxLines: 2,
-                            softWrap: true,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(fontWeight: FontWeight.bold),
+                      const SizedBox(height: 16),
+                      Text(
+                        movie.description,
+                        maxLines: 8,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: .center,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              '${movie.releaseDate} | ${movie.director} | ${movie.producer}',
+                              maxLines: 2,
+                              softWrap: true,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
